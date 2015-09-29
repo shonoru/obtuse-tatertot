3a3281bf7fb17c916c07fdadd945e0e6bacf60e6
Andrey Bodoev <shonoru@gmail.com>
2015-09-29 20:36:16 +0800

    Add exercise 5

---
+++ everything-is-an-object/ex5/DataOnly.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class DataOnly {
+  static int i;
+  static double d;
+  static boolean b;
+
+  public static void main(String[] args) {
+    i = 10;
+    System.out.println(i);
+
+    d = 10.4f;
+    System.out.println(d);
+
+    b = true;
+    System.out.println(b);
+  }
+}
