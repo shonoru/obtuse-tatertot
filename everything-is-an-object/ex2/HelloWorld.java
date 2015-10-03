83815485e06365ad5852d3068f4a20cef864f515
Andrey Bodoev <shonoru@gmail.com>
2015-10-03 19:49:22 +0800

    Add exercise 2 and 15 on top of 2
    * Add generated documentation

---
+++ everything-is-an-object/ex2/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author John Snow
+ * @version 0.1
+ * @since 2015-10-03
+ */
+public class HelloWorld {
+  /**
+	 * Default method
+	 */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
