e5a5b64f4f3206826dfff8e9c74c53885903edbd
Andrey Bodoev <shonoru@gmail.com>
2015-10-04 17:28:01 +0800

    Add CalcVelocity class, exercise 4

---
+++ operators/ex4/CalcVelocity.java
@@ -0,0 +1,10 @@
+class CalcVelocity {
+  static int distance = 10;
+  static int time = 2;
+
+  public static void main(String[] args) {
+    int speed = distance / time;
+
+    System.out.println(speed);
+  }
+}
