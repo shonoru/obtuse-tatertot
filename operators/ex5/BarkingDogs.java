182ad8391e47d836c87620adff24988597f5c88c
Andrey Bodoev <shonoru@gmail.com>
2015-10-11 15:22:59 +0800

    Add exercise 6, test for comparison for all references

--- operators/ex5/BarkingDogs.java
+++ operators/ex5/BarkingDogs.java
@@ -12,7 +12,18 @@ class BarkingDogs {
     dog2.name = "scruffy";
     dog2.says = "Wurf!";
 
+    Dog spot = new Dog();
+    spot.name = "spot";
+    spot.says = "Ruff!";
+
     System.out.println(dog1.name + ": " + dog1.says);
     System.out.println(dog2.name + ": " + dog2.says);
+
+    System.out.println(dog1.equals(spot));
+    System.out.println(dog1 == spot);
+    System.out.println(dog1.name == spot.name);
+    System.out.println(dog1.says == spot.says);
+
+    System.out.println(dog1.equals(dog2));
   }
 }
